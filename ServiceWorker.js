161b1baa49033f7a5349253ab3235d8053df9268
PedroGustavoBugaboo <spg2616@gmail.com>
2025-06-13 18:29:22 -0300

    build 1.0

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Unimed Fortaleza-Jornada Unimed-1.0.1";
+const contentToCache = [
+    "Build/Jornada.loader.js",
+    "Build/Jornada.framework.js.gz",
+    "Build/Jornada.data.gz",
+    "Build/Jornada.wasm.gz",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
